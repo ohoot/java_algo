bb11673029c99f88cee92b979d374cc87deef1ca
Hyeonwoo Joo <ohootkk@gmail.com>
2022-04-22 02:28:18 +0900

    Completed Network

--- src/main/quizzes/Network.java
+++ src/main/quizzes/Network.java
@@ -1,2 +1,30 @@
-package main.quizzes;public class Network {
+package main.quizzes;
+
+public class Network {
+    public int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[n];
+
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, visited, computers);
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public void dfs(int n, boolean[] visited, int[][] computers) {
+        if (visited[n]) return;
+
+        visited[n] = true;
+
+        for (int i = 0; i < computers[n].length; i++) {
+            if (n != i && computers[n][i] == 1) {
+                dfs(i, visited, computers);
+            }
+        }
+    }
 }
