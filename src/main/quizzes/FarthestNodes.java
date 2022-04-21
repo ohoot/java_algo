43ffc5a0406a02a9ef6f0949842d1510d0e0f69b
Hyeonwoo Joo <ohootkk@gmail.com>
2022-04-21 15:47:59 +0900

    Added FarthestNodes

--- src/main/quizzes/FarthestNodes.java
+++ src/main/quizzes/FarthestNodes.java
@@ -1,23 +1,8 @@
-package test;
-
-import org.junit.jupiter.api.Test;
+package main.quizzes;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
-public class CommonTest {
-    @Test
-    public void test() {
-        int[] aa = new int[3];
-
-        int i = 3;
-        int j = i = aa[0] = 4;
-
-        System.out.println(j);
-
-    }
-
-    @Test
+public class FarthestNodes {
     public int solution(int n, int[][] edge) {
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(1);
