0a70a3be9af608f2e1cbdfe3c90b37ec871a6cb6
Hyeonwoo Joo <ohootkk@gmail.com>
2022-04-22 11:02:50 +0900

    Completed TapeEquilibrium

--- src/main/quizzes/TapeEquilibrium.java
+++ src/main/quizzes/TapeEquilibrium.java
@@ -21,4 +21,28 @@ public class TapeEquilibrium {
 
         return min;
     }
+
+    public int betterSolution(int[] A) {
+        int sum_fromFirst = 0;
+        int sum_toLast = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            sum_toLast += A[i];
+        }
+
+        int minDiff = Integer.MAX_VALUE;
+
+        for (int p = 1; p < A.length; p++) {
+            sum_fromFirst += A[p - 1];
+            sum_toLast -= A[p - 1];
+
+            int diff = Math.abs(sum_fromFirst - sum_toLast);
+
+            if (diff < minDiff) {
+                minDiff = diff;
+            }
+        }
+
+        return minDiff;
+    }
 }
